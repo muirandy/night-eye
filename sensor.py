0d7714ee6f8a9f3310e4ca72c5e13a2a2755e898
Andy Muir <muirandy@yahoo.co.uk>
2020-09-26 15:38:40 +0100

    take picture when sensor detects movement

--- sensor.py
+++ sensor.py
@@ -1,8 +1,11 @@
 from gpiozero import MotionSensor
+from picamera import PiCamera
 from time import sleep
 
+camera = PiCamera()
 sensor = MotionSensor(4)
 
-sleep(10)
+#sleep(10)
 sensor.wait_for_motion()
 print("dog")
+camera.capture('/home/pi/Desktop/sensor5.jpg')
